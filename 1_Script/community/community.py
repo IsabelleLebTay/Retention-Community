4c29c0c1b3edb130667f81f8c1da0dcf17212941
Isabelle Lebeuf-Taylor <lebeufta@ualberta.ca>
2024-04-17 12:32:59 -0600

    init percept truncation

--- 1_Script/community/community.py
+++ 1_Script/community/community.py
@@ -2,35 +2,35 @@ import pandas as pd
 import csv
 import os
 
-raw_Path = r"C:\Users\ilebe\Documents\!Masters!\RETN Community\0_Data\Raw\Wildtrax Patch Retention"
+raw_Path = r"C:\Users\ilebe\Documents\!Masters!\RETN Community\0_Data\Raw\Wildtrax Limited Percept"
 processed_Path = r"C:\Users\ilebe\Documents\!Masters!\RETN Community\0_Data\Processed"
 # Set your working directory
-data = pd.read_csv(os.path.join(raw_Path,"BU_Harvest_Patch_Retention_-_Community_2023_main_report.csv"))
+data = pd.read_csv(os.path.join(raw_Path,"BU_Harvest_Patch_Retention_-_Community_&_Limited_perceptibility_main_report.csv"))
 
 #data['point_count'] = data['location'] + " " +  data['recording_date'] + " " +data['recording_time']
 
 # dummies from the species code column, drops column
 
 data = data[data['aru_task_status'] == 'Transcribed']
-# data['dummies'] = data['species_code']
-data['dummies'] = data['species_common_name']
+data['dummies'] = data['species_code']
+# data['dummies'] = data['species_common_name']
  #species_common_name
 # full_species_list = list(data['species_common_name'].unique())
 
 abundance = pd.get_dummies(data=data, columns= ['dummies'], prefix='', prefix_sep='')
 
 # add the species code column back
-# df = pd.concat([data['species_code'], abundance], axis=1) 
+df = pd.concat([data['species_code'], abundance], axis=1) 
 
 # with open(os.path.join(raw_Path,'columnsList.csv'), newline='') as f:
 #     reader = csv.reader(f)
 #     columnsList = list(reader)[0]
 
-# species_list = list(data['species_code'].unique())
-full_species_list = list(data['species_common_name'].unique())
+full_species_list = list(data['species_code'].unique())
+# full_species_list = list(data['species_common_name'].unique())
 
-exclude_these = ['NONE', 'Red Squirrel', "Red-tailed Hawk", 'Unidentified Woodpecker', 'Unidentified signal', 'Unidentified Passerine', 'Unidentified Poecile (Chickadee)', 'Unidentified bird', 'Unidentified Sparrow', 'Unidentified Trill', 'Unidentified Thrush', 'Unidentified Mammal', 'Unidentified Warbler', 'Unidentified Flycatcher', 'Unidentified Vireo', 'Unidentified Corvid', 'Unidentified Tern']
-# exclude_these = ['NONE', 'RESQ', "RTHA", 'UNWO', 'UNKN', 'UNPA', 'UPCH', 'UNBI', 'UNSP', 'UNTR', 'UNTH', 'UNMA', 'UNWA', 'UNFL', 'UNVI',  'UNCV', 'UNTE']
+# exclude_these = ['NONE', 'Red Squirrel', "Red-tailed Hawk", 'Unidentified Woodpecker', 'Unidentified signal', 'Unidentified Passerine', 'Unidentified Poecile (Chickadee)', 'Unidentified bird', 'Unidentified Sparrow', 'Unidentified Trill', 'Unidentified Thrush', 'Unidentified Mammal', 'Unidentified Warbler', 'Unidentified Flycatcher', 'Unidentified Vireo', 'Unidentified Corvid', 'Unidentified Tern']
+exclude_these = ['NONE', 'RESQ', "RTHA", 'UNWO', 'UNKN', 'UNPA', 'UPCH', 'UNBI', 'UNSP', 'UNTR', 'UNTH', 'UNMA', 'UNWA', 'UNFL', 'UNVI',  'UNCV', 'UNTE']
 
 select_species = [i for i in full_species_list if i not in exclude_these]
 
@@ -60,4 +60,4 @@ print(df1.head())
 grouped = df1.groupby('location').agg(columns_dictA)
 print(grouped.columns)
 print(grouped.head())
-grouped.to_csv(os.path.join(processed_Path,'community_abundance_by_location_common_names.csv'))
+grouped.to_csv(os.path.join(processed_Path, "Limited perceptibility", 'community_occupancy_by_location_code_names.csv'))
---
+++ README.md
@@ -0,0 +1,3 @@
+# Small-scale residual retention: who uses it anyway?
+
+Bird community analysis of residual retention use in harvests iwth the first 22 years of regeneration, in boreal Alberta.
